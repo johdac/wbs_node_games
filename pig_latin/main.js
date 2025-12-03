87f2d36076916741086f2ac6b65dcf8664092410
Johannes Dach <johannesdach@gmail.com>
2025-12-03 22:00:04 +0100

    piglatin with lower cased words. has issue with all caps

---
+++ pig_latin/main.js
@@ -0,0 +1,30 @@
+const args = process.argv.slice(2);
+let words = args[0].split(" ");
+
+const capitalizeWord = (word) => {
+  if (word) return word[0].toUpperCase() + word.slice(1);
+};
+
+words.map((word) => {
+  // We remember if the word was capitalized but work on lower cased strings
+  const wordWasUpper = word === capitalizeWord(word);
+  word = word.toLowerCase();
+
+  // Per default we return the original word (for non alpha chars)
+  let newWord = word;
+
+  // We check each case with regex
+  let matched;
+  matched = word.match(/^([bcdfghjklmnpqrstvwxz])([aeiouy])(.*$)/);
+  if (matched) newWord = matched[2] + matched[3] + matched[1] + "ay";
+
+  matched = word.match(/^([bcdfghjklmnpqrstvwxz])([bcdfghjklmnpqrstvwxz])(.*$)/);
+  if (matched) newWord = matched[3] + matched[1] + matched[2] + "ay";
+
+  matched = word.match(/^([aeiouy])(.*$)/);
+  if (matched) newWord = matched[0] + "way";
+
+  // Restore capitalization
+  if (wordWasUpper) newWord = capitalizeWord(newWord);
+  process.stdout.write(`${newWord} `);
+});
